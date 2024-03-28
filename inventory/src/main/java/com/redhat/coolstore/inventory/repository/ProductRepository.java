30a9bd3f1b39497f793744440855e922a4528a9c
Daniel Oh <doh@redhat.com>
2024-03-27 22:06:23 -0400

    Update Quarkus 3.9.1

--- inventory/src/main/java/com/redhat/coolstore/inventory/repository/ProductRepository.java
+++ inventory/src/main/java/com/redhat/coolstore/inventory/repository/ProductRepository.java
@@ -1,13 +1,9 @@
 package com.redhat.coolstore.inventory.repository;
 
 import java.util.List;
-
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-
 import org.jboss.logging.Logger;
 import com.redhat.coolstore.inventory.model.Product;
-
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.panache.common.Page;
 import io.quarkus.panache.common.Sort;
@@ -15,14 +11,14 @@ import io.quarkus.panache.common.Sort;
 @ApplicationScoped
 public class ProductRepository implements PanacheRepository<Product> {
 
-	private static Logger logger = Logger.getLogger( ProductRepository.class.getName() );
-	
+	private static Logger logger = Logger.getLogger(ProductRepository.class.getName());
+
 	public Product findById(Long id) {
 		logger.debug("Entering ProductRepository.findById()");
 		Product p = find("id", id).firstResult();
 		return p;
 	}
-	
+
 	public List<Product> findAll(Page page, Sort sort) {
 		logger.debug("Entering ProductRepository.findAll()");
 		List<Product> p = Product.findAll(sort)
